b39df53b24531cc10e80f37b1d9178ed73f21e4e
cindych419@gmail.com <cindych419@gmail.com>
2018-05-03 04:39:02 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,7 +3,7 @@ def roll_call_dwarves(dwarves)
 end
 
 def summon_captain_planet(planeteer_calls)# code an argument here
-  planeteer_calls.map {|planeteer| planeteer.capitalize!}# Your code here
+  planeteer_calls.map {|planeteer| planeteer.capitalize+"!"}# Your code here
 end
 
 def long_planeteer_calls# code an argument here
